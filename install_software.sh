44ce8de37d5b5db604bdcdf3a88f69d63fc9fd0c
Adam Spontarelli <adam@vector-space.org>
2016-11-17 09:34:08 -0500

    moved files

--- install_software.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp hosts /etc/ansible/
-ansible-playbook --become --ask-become-pass -u adam software.yml
--- timeout.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo 'export TMOUT=240' > /etc/profile.d/timeout.sh
-chmod 755 /etc/profile.d/timeout.sh
