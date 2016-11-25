15a9e1e63700ee5866b24fa87b88974d2a8b2dea
Adam Spontarelli <spontarelliam@gmail.com>
2016-11-25 15:44:16 -0500

    added idle logoug user

---
+++ scripts/idle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# logout any user who has been idle for at least 1 day
+
+idle=$(w | grep -G ' [1-9][0-9]:[0-9][0-9] ' | awk '{print $1}')
+idle=$(w | grep -G ' [1-9]days ' | awk '{print $1}')
+for user in $idle
+do
+    echo $user
+    # pkill -u $user
+done
+
+# long names get truncated
+full_name=`who | grep -E -o '$user\w+ '`
+echo $full_name
