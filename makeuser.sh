1c26c21fcf2781f7a8ff852fe67f9d950a308b38
Adam Spontarelli <adam@vector-space.org>
2018-09-19 12:44:49 -0400

    so many changes

--- makeuser.sh
+++ makeuser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 username=$1
-sudo useradd $username -G lock,uucp,dialout,member
+sudo useradd $username -G lock,uucp,dialout,member,optical,lp,power,network,storage
 sudo mkdir /vsfs01/home/${username}
 sudo chown -R ${username}:member /vsfs01/home/${username}
 echo "${username}:temp123" | sudo chpasswd
