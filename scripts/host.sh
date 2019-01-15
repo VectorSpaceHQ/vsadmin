c9c0c74a3a2c222ce5fca186a97addf7ec1940d9
Adam Spontarelli <adam@vector-space.org>
2019-01-15 15:05:45 -0500

    added initialize_client.yml that sets up openssh and public key on new client

--- scripts/host.sh
+++ scripts/host.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+ansible-playbook -i ../hosts -u root -K ../playbooks/initialize_client.yml -v --ask-pass
 ansible-playbook -i ../hosts -u root -K ../playbooks/run_all.yml -vvv > run_all.log 2>&1
