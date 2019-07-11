3f4bf1c3cdda51acbb4ce658a3c501b614ee5360
Adam Spontarelli <adam@vector-space.org>
2019-07-11 14:28:56 -0400

    remove old makeuser

--- makeuser.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-username=$1
-sudo useradd $username -G lock,uucp,dialout,member,optical,lp,power,network,storage
-sudo mkdir /vsfs01/home/${username}
-sudo chown -R ${username}:member /vsfs01/home/${username}
-echo "${username}:temp123" | sudo chpasswd
-cd /var/yp
-sudo make
