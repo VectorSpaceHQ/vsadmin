2de76a36101fd2da1248ca1b86c595950670b78f
Adam Spontarelli <adam@vector-space.org>
2019-11-20 10:47:43 -0500

    added lightburn config files

--- scripts/host.sh
+++ scripts/host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh-copy-id -i ~/.ssh/id_rsa.pub adam@laser
+# ssh-copy-id -i ~/.ssh/id_rsa.pub adam@laser
 
 ansible-playbook -i ../hosts -u adam -K ../playbooks/initialize_client.yml --ask-pass
 ansible-playbook -i ../hosts -u adam -K ../playbooks/run_all.yml
