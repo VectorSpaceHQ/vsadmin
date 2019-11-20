951f0d3611ada47849834ecf01dae3dd9dd7537f
Adam Spontarelli <adam@vector-space.org>
2019-11-20 09:21:09 -0500

    ssh key now copied from host instead of client

--- scripts/client.sh
+++ scripts/client.sh
@@ -12,7 +12,8 @@ systemctl start sshd
 # copy public key
 mkdir -p ~/.ssh
 
-cat ../id_rsa.pub >>  ~/.ssh/authorized_keys
+# cat ../id_rsa.pub >>  ~/.ssh/authorized_keys
+# this is now done from the host using ssh-copy-id
 
 # Add group sudo to sudoers file
 sed -i 's/# %sudo/%sudo/g' /etc/sudoers
--- scripts/host.sh
+++ scripts/host.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+ssh-copy-id -i ~/.ssh/id_rsa.pub adam@laser
+
 ansible-playbook -i ../hosts -u adam -K ../playbooks/initialize_client.yml --ask-pass
 ansible-playbook -i ../hosts -u adam -K ../playbooks/run_all.yml
 # ansible-playbook -i ../hosts -u adam -K ../playbooks/reboot.yml -v
