16b8345411656a7cd0b77a2f995b216795329922
Adam Spontarelli <adam@vector-space.org>
2026-01-12 14:28:29 -0500

    more work on xfce.yml

---
+++ config_files/xfce-per-user-configs.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -e
+
+CHANNEL_SHORTCUTS="xfce4-keyboard-shortcuts"
+SESSION_CHANNEL="xfce4-session"
+PANEL_XML="$HOME/.config/xfce4/xfce-perchannel-xml/xfce4-panel.xml"
+
+echo "Applying XFCE shortcuts, tiling, power settings, and user menu options..."
+
+# ----------------------------
+# KEYBOARD SHORTCUTS
+# ----------------------------
+
+xfconf-query -c $CHANNEL_SHORTCUTS \
+  -p "/commands/custom/<Control><Alt><Shift>w" -n -t string -s "xfce4-popup-whiskermenu"
+
+xfconf-query -c $CHANNEL_SHORTCUTS \
+  -p "/commands/custom/<Control><Alt>t" -n -t string -s "xfce4-terminal"
+
+xfconf-query -c $CHANNEL_SHORTCUTS \
+  -p "/commands/custom/<Control><Alt>l" -n -t string -s "xflock4"
+
+xfconf-query -c $CHANNEL_SHORTCUTS \
+  -p "/commands/custom/<Print>" -n -t string -s "xfce4-screenshooter"
+
+# ----------------------------
+# WINDOW TILING (XFWM4)
+# ----------------------------
+
+xfconf-query -c xfwm4 -p /general/tile_left -n -t string -s "<Super>Left"
+xfconf-query -c xfwm4 -p /general/tile_right -n -t string -s "<Super>Right"
+
+# ----------------------------
+# POWER MANAGEMENT SETTINGS
+# ----------------------------
+
+xfconf-query -c xfce4-power-manager \
+  -p "/xfce4-power-manager/inactivity-on-ac" -n -t int -s 0
+
+# ----------------------------
+# USER MENU OPTIONS (xfce4-session)
+# ----------------------------
+
+# Hide standard buttons via xfconf
+xfconf-query -c $SESSION_CHANNEL -p "/shutdown/ShowShutdown" -n -t bool -s false
+xfconf-query -c $SESSION_CHANNEL -p "/shutdown/ShowSuspend" -n -t bool -s false
+xfconf-query -c $SESSION_CHANNEL -p "/shutdown/ShowSwitchUser" -n -t bool -s false
+
+# Show only "Log Out" without prompt
+xfconf-query -c $SESSION_CHANNEL -p "/shutdown/ShowLogout" -n -t bool -s true
+xfconf-query -c $SESSION_CHANNEL -p "/shutdown/ShowLogoutPrompt" -n -t bool -s false
+xfconf-query -c xfce4-session -p /general/PromptOnLogout -n -t bool -s false
+
+# Disable XFCE monitor layout auto-apply (prevents "xfdesktop assign layout" popup)
+#rm -rf ~/.config/xfce4/desktop/
+# Is this destroying the desktop shortcuts?
+#rm -f ~/.config/xfce4/desktop/icons*yaml
+xfconf-query -c displays -p /Notify -n -t int -s 0
+xfconf-query -c displays -p /AutoApply -n -t bool -s true
+xfconf-query -c xfce4-desktop -p /desktop-icons/style -n -t int -s 2
+#xfconf-query -c xfce4-desktop -p /desktop-icons/last-monitor-layout -n -t int -s 0
+
+# ----------------------------#
+# Remove some options from panel
+# ----------------------------#
+# Ensure plugin-2 is of type "actions"
+xfconf-query -c xfce4-panel -p /plugins/plugin-2 -n -t string -s "actions"
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/items -r -R || true
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/items -n \
+	     -t string -s "+lock-screen" \
+	     -t string -s "-switch-user" \
+	     -t string -s "+separator" \
+	     -t string -s "-suspend" \
+	     -t string -s "-hibernate" \
+	     -t string -s "-hybrid-sleep" \
+	     -t string -s "-separator" \
+	     -t string -s "-shutdown" \
+	     -t string -s "+restart" \
+	     -t string -s "+separator" \
+	     -t string -s "+logout" \
+	     -t string -s "-logout-dialog"
+# ----------------------------
+
+
+echo "All XFCE settings applied."
+echo "You may need to log out and log back in, or restart the panel (xfce4-panel -r) for all changes to fully take effect."
